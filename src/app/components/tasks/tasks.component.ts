ab6ecc6c93c9b2a8d2b68a761afa3ccee4864930
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2024-03-07 20:58:18 +0100

    playing with the child and parent components

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { TASKS } from '../../mock-task';
+import { Task } from '../../Task';
 
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
 })
-export class TasksComponent {}
+export class TasksComponent {
+  tasks: Task[] = TASKS;
+}
